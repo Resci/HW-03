d7ed8bb0466d58bc9a010f2a2fce455fc0485202
Resci <vIsUAl123>
2021-01-15 15:15:15 +0200

    Delete comment in taxi_schema.sql

--- src/main/resources/taxi_schema.sql
+++ src/main/resources/taxi_schema.sql
@@ -1,19 +1,3 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : Mate
- Source Server Type    : MySQL
- Source Server Version : 80022
- Source Host           : localhost:3306
- Source Schema         : taxi_schema
-
- Target Server Type    : MySQL
- Target Server Version : 80022
- File Encoding         : 65001
-
- Date: 15/01/2021 01:09:47
-*/
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
