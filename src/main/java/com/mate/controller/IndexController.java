3b649e3cc178ab5e507e902f95538f3c1515882a
Resci <vIsUAl123>
2021-01-21 13:31:48 +0200

    Change buttons to links
    (cherry picked from commit bee48fa2f39d8228239ba5c929c26cd87919c002)

--- src/main/java/com/mate/controller/IndexController.java
+++ src/main/java/com/mate/controller/IndexController.java
@@ -12,30 +12,4 @@ public class IndexController extends HttpServlet {
             throws ServletException, IOException {
         req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
     }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
-        if (req.getParameter("All drivers") != null) {
-            resp.sendRedirect("/drivers/all");
-        }
-        if (req.getParameter("All cars") != null) {
-            resp.sendRedirect("/cars/all");
-        }
-        if (req.getParameter("All manufacturers") != null) {
-            resp.sendRedirect("/manufacturers/all");
-        }
-        if (req.getParameter("Add driver") != null) {
-            resp.sendRedirect("/drivers/add");
-        }
-        if (req.getParameter("Add manufacturer") != null) {
-            resp.sendRedirect("/manufacturers/add");
-        }
-        if (req.getParameter("Add car") != null) {
-            resp.sendRedirect("/cars/add");
-        }
-        if (req.getParameter("Add driver to car") != null) {
-            resp.sendRedirect("/cars/drivers/add");
-        }
-    }
 }
