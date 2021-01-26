84e3f60ed743e113cb097589fc68669f8ed373e2
Resci <vIsUAl123>
2021-01-26 06:02:47 +0200

    Simplify login() method

--- src/main/java/com/mate/security/AuthenticationServiceImpl.java
+++ src/main/java/com/mate/security/AuthenticationServiceImpl.java
@@ -2,10 +2,10 @@ package com.mate.security;
 
 import com.mate.exception.AuthenticationException;
 import com.mate.lib.Inject;
-import com.mate.lib.Injector;
 import com.mate.lib.Service;
 import com.mate.model.Driver;
 import com.mate.service.DriverService;
+import java.util.Optional;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
@@ -14,11 +14,10 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public Driver login(String login, String password) throws AuthenticationException {
-        Driver driver = driverService.findByLogin(login).orElseThrow(
-                () -> new AuthenticationException("Incorrect password or login"));
-        if (driver.getPassword().equals(password)) {
-            return driver;
+        Optional<Driver> driverFromDB = driverService.findByLogin(login);
+        if (driverFromDB.isPresent() && driverFromDB.get().getPassword().equals(password)) {
+            return driverFromDB.get();
         }
-        throw new AuthenticationException("Incorrect password or login");
+        throw new AuthenticationException("Incorrect login or password");
     }
 }
